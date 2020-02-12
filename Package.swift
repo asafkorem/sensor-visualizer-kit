4d06a3bc074d53177e73b729770d85b670d9725a
Joe Blau <im@joeblau.com>
2020-02-11 21:13:29 -0800

    Move files back

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "SensorVisualizerKit",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
     ]
 )
