707109df2993a6aa5a837903ab9bdf18de4db9d9
Tiago Rodrigues <tiagorodrigues@Tiagos-iMac.lan>
2020-06-02 15:16:44 +0100

    Set SensorVisualizerWindow access control to open

--- Sources/SensorVisualizerWindow.swift
+++ Sources/SensorVisualizerWindow.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class SensorVisualizerWindow: UIWindow {
+open class SensorVisualizerWindow: UIWindow {
 
     public weak var sensorVisualizerWindowDelegate: SensorVisualizerWindowDelegate?
 
