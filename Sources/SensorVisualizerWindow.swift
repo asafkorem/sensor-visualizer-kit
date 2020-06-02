da21c7a91bc666753d3c4627306802e0d83b3dab
Tiago Rodrigues <tiagorodrigues@Tiagos-iMac.lan>
2020-06-02 16:33:07 +0100

    Missing public for a required init

--- Sources/SensorVisualizerWindow.swift
+++ Sources/SensorVisualizerWindow.swift
@@ -60,7 +60,7 @@ open class SensorVisualizerWindow: UIWindow {
         }
     }
 
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
